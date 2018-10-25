17b61f7e7b68ee33c3a2f796213dc77aa370b867
vspande13 <vspande4@gmail.com>
2018-10-25 10:22:05 +0530

    app property added

--- SimpleCrudProj/src/main/java/com/controller/HomeController.java
+++ SimpleCrudProj/src/main/java/com/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 
+import com.serviceInterface.ServiceInterface;
+
 @Controller
 public class HomeController {
-
+ServiceInterface service;
 }
